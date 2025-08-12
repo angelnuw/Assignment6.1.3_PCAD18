fcaaf74df7c6ff7347e0908a2e5a09d5eac4d388
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-12 13:16:29 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment6._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            MoveZeroes(nums);
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+
+        static void MoveZeroes(int[] nums)
+        {
+            int insertPos = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[insertPos] = nums[i];
+                    insertPos++;
+                }
+            }
+
+            while (insertPos < nums.Length)
+            {
+                nums[insertPos] = 0;
+                insertPos++;
+            }
+        }
+    }
+}
